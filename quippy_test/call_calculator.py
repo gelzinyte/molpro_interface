07cc4f7fe00337c5c4da74f2fd2c3544e332efc1
Elena Gelzinyte <elena.gelzinyte@gmail.com>
2020-05-07 14:50:25 +0100

    upload file

---
+++ quippy_test/call_calculator.py
@@ -0,0 +1,17 @@
+from ase.io import read
+from quippy.potential import Potential
+from ase.io.extxyz import key_val_dict_to_str
+import pdb
+
+# example of how to call molpro calculator
+
+calc_args = { 'template' : '{/home/eg475/molpro_stuff/driver_tests/quippy_test/template_e_f.inp}',
+              'molpro' : '{/opt/molpro/bin/molprop}',
+              'energy_from' : 'RKS',
+              'extract_forces' : True}
+
+molpro = Potential(args_str='FilePot command=/home/eg475/molpro_stuff/driver_tests/molpro_driver.py', calc_args=calc_args)
+
+methane = read('methane.xyz')
+methane.set_calculator(molpro)
+print('finally calculated energy:', methane.get_potential_energy())
